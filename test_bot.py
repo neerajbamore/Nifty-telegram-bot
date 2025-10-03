c6c5757c1f39c40bfa1722adc89bcbeda410db5f
neerajbamore <neerajbamore@gmail.com>
2025-10-03 17:00:48 +0530

    test_bot.py

---
+++ test_bot.py
@@ -0,0 +1,13 @@
+import os
+import requests
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+def send_message(text):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    data = {"chat_id": CHAT_ID, "text": text}
+    response = requests.post(url, data=data)
+    print("Telegram response:", response.json())
+
+send_message("🚀 Test script se message aa raha hai ✅")
